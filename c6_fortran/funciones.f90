a485aea06e3643c1e97d43909ea38cab07f042ae
LeninPA <lenin.pavon@comunidad.unam.mx>
2024-08-25 10:24:00 -0600

    FEAT: Programa funciones

--- c6_fortran/funciones.f90
+++ c6_fortran/funciones.f90
@@ -1,47 +1,48 @@
 MODULE aritmetica
         IMPLICIT NONE
-        REAL, PARAMETER :: tau = 6.28
+        REAL, PARAMETER :: tau = 6.28, pi = 3.14159265359,&
+        phi = 1.168033, euler = 2.71828, square = 1.41421
         contains
-        REAL FUNCTION suma(a, b)
+        REAL FUNCTION suma(b)
             IMPLICIT NONE
-            REAL, INTENT(IN) :: a,b
-            suma = a + b
+            REAL, INTENT(IN) :: b
+            suma = tau + b
         END FUNCTION suma
-        REAL FUNCTION resta(a, b)
+        REAL FUNCTION resta(b)
             IMPLICIT NONE
-            REAL, INTENT(IN) :: a,b
-            resta = a - b
+            REAL, INTENT(IN) :: b
+            resta = tau - b
         END FUNCTION resta
-        REAL FUNCTION multiplicacion(a, b)
+        REAL FUNCTION multiplicacion(b)
             IMPLICIT NONE
-            REAL, INTENT(IN) :: a,b
-            multiplicacion = a * b
+            REAL, INTENT(IN) :: b
+            multiplicacion = tau * b
         END FUNCTION multiplicacion
-        REAL FUNCTION division(a, b)
+        REAL FUNCTION division(b)
             IMPLICIT NONE
-            REAL, INTENT(IN) :: a,b
+            REAL, INTENT(IN) :: b
             if ( b == 0.0 ) then
-                division = a
+                division = tau 
             else
-                division = a / b
+                division = tau / b
             endif
         END FUNCTION division
 END MODULE aritmetica
 PROGRAM funciones
         USE aritmetica
         IMPLICIT NONE
-        REAL :: a = 0,b = 0 
-        PRINT *,"Ingresa un real a:"
-        READ(*,'(F5.2)') a 
+        REAL :: b = 0!,a=0 
+        !PRINT *,"Ingresa un real a:"
+        !READ(*,'(F5.2)') a 
         PRINT *,"Ingresa un real b:"
         READ(*,'(F5.2)') b 
         PRINT *,"Escribiste"
-        PRINT *,"a:", a
+        !PRINT *,"a:", a
         PRINT *,"b:", b
         PRINT *,"---------------------------"
-        PRINT *,"a + b :", suma(a,b)
-        PRINT *,"a - b :", resta(a,b)
-        PRINT *,"a * b :", multiplicacion(a,b)
-        PRINT *,"a / b :", division(a,b)
+        PRINT *,"tau + b :", suma(b)
+        PRINT *,"tau - b :", resta(b)
+        PRINT *,"tau * b :", multiplicacion(b)
+        PRINT *,"tau / b :", division(b)
         PRINT *,"---------------------------"
 END PROGRAM funciones
