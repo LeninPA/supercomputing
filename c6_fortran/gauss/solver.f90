b866cf5cc07715a5bb2a7b9766a5e1ccf4ca9d44
LeninPA <lenin.pavon@comunidad.unam.mx>
2024-08-29 05:44:54 -0600

    FEAT: Reading data

--- c6_fortran/gauss/solver.f90
+++ c6_fortran/gauss/solver.f90
@@ -94,4 +94,58 @@ end module gauss_jordan
 program solver
     use gauss_jordan
     implicit none
+    !
+    ! Parameters
+    !
+    integer :: n                        ! Matrix dimension
+    integer :: i,j,choice               ! Aux variables
+    real, allocatable :: A(:,:), b(:)   ! Matrix and vector of solutions
+    logical :: solution                 ! Boolean solution exists
+    character(len=100) :: filename      ! Path to matrix file if choice 2
+    !
+    ! CLI
+    !
+    print *, "Ax=b solver by Gauss - Jordan"
+    print '(A,/,A,/,A)', "1. Interactive", "2. Input from file", "Choice: "
+    read *, choice
+    if ( choice == 1) then
+        ! Interactive mode
+        print '(A,/,A)', "You chose interactive CLI", "Enter dimension of the square matrix: "
+        read *,n
+        if ( n > 0 ) then
+            allocate(A(n,n), b(n))
+        else
+            stop "Non-positive dimension introduced, terminating program"
+        end if
+        ! Matrix 
+        print *, "Input the entries of A (row by row):"
+        do i = 1,n
+            print *, "Row ", i, ":"
+            read *, A(i, :)
+            print *, "Row ", i, ":", A(i, :)
+        end do
+        ! Results
+        print *, "Input the entries of b:"
+        read *, b
+        print *, "b:", b
+    elseif( choice == 2 ) then
+        ! stop "Not working correctly as of yet"
+        ! Input from file
+        print '(A,/,A)', "You chose reading from file", "Enter file path: "
+        read *, filename
+        print *, filename
+        ! Reading filename
+        open(unit = 10, file = filename, status = 'old', action = 'read')
+        read(10, *) n
+        allocate( A(n,n) , b(n) )
+        do i = 1,n
+            read(10, *) A(i,:)
+            print *, "Column", i, ":", A(i, :)
+        end do
+        read(10, *) b
+        print *, "b:", b
+        close(10)
+    else
+        stop "Invalid choice"
+    end if
 end program solver
